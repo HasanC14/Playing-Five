9b56b16fb31b5f4c8da8edacd635598b912ea098
HasanC14 <chowdhuryh999@gmail.com>
2022-08-22 11:13:38 +0600

    player select limit

--- JavaScript/common.js
+++ JavaScript/common.js
@@ -1,5 +1,9 @@
 function display(playerID) {
-    const NewElement = document.createElement("li");
-    NewElement.innerText = document.getElementById(playerID).innerText;
-    document.getElementById("display-ol").appendChild(NewElement);
+    const count = document.getElementsByTagName('li').length;
+    if (count < 5) {
+        const NewElement = document.createElement("li");
+        NewElement.innerText = document.getElementById(playerID).innerText;
+        document.getElementById("display-ol").appendChild(NewElement);
+    }
+
 }
