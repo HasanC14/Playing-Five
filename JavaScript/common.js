449b28bd6899a8e349d2945ce64db4bd85340a2e
HasanC14 <chowdhuryh999@gmail.com>
2022-08-22 21:01:36 +0600

    per player budget calculation

--- JavaScript/common.js
+++ JavaScript/common.js
@@ -1,9 +1,10 @@
-function display(playerID) {
+function display(playerID, playerBtn) {
     const count = document.getElementsByTagName('li').length;
     if (count < 5) {
         const NewElement = document.createElement("li");
         NewElement.innerText = document.getElementById(playerID).innerText;
         document.getElementById("display-ol").appendChild(NewElement);
+        document.getElementById(playerBtn).disabled = true;
     }
-
+    else alert("You Cannot Select More than Five Player");
 }
--- JavaScript/utilities.js
+++ JavaScript/utilities.js
@@ -1,28 +1,34 @@
 /*--------------Player Selection----------------*/
 document.getElementById('btn-player-select-1').addEventListener("click", function () {
-    display('player-name-1')
+    display('player-name-1', 'btn-player-select-1')
 });
 document.getElementById('btn-player-select-2').addEventListener("click", function () {
-    display('player-name-2')
+    display('player-name-2', 'btn-player-select-2')
 });
 document.getElementById('btn-player-select-3').addEventListener("click", function () {
-    display('player-name-3')
+    display('player-name-3', 'btn-player-select-3')
 });
 document.getElementById('btn-player-select-4').addEventListener("click", function () {
-    display('player-name-4')
+    display('player-name-4', 'btn-player-select-4')
 });
 document.getElementById('btn-player-select-5').addEventListener("click", function () {
-    display('player-name-5')
+    display('player-name-5', 'btn-player-select-5')
 });
 document.getElementById('btn-player-select-6').addEventListener("click", function () {
-    display('player-name-6')
+    display('player-name-6', 'btn-player-select-6')
 });
 document.getElementById('btn-player-select-7').addEventListener("click", function () {
-    display('player-name-7')
+    display('player-name-7', 'btn-player-select-7')
 });
 document.getElementById('btn-player-select-8').addEventListener("click", function () {
-    display('player-name-8')
+    display('player-name-8', 'btn-player-select-8')
 });
 document.getElementById('btn-player-select-9').addEventListener("click", function () {
-    display('player-name-9')
+    display('player-name-9', 'btn-player-select-9')
+});
+/*--------------Budget Calculation---------------*/
+document.getElementById('PlayerExpensesCalculate').addEventListener("click", function () {
+    const count = document.getElementsByTagName('li').length;
+    const input = parseInt(document.getElementById('PerPlayer').value);
+    document.getElementById('PlayerExpenses').innerText = input * count;
 });
